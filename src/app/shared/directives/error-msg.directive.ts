8219977f727ef4b3d67300b307e0454de38ed699
Adrian Fernandez <adrianxtuu@gmail.com>
2023-01-25 11:05:41 -0400

    [UPDATE]

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -7,6 +7,7 @@ export class ErrorMsgDirective implements OnInit {
 
   htmlElement: ElementRef<HTMLElement>
   @Input() color: string = 'red'
+  @Input() mensaje: string = 'Debe ingresar este campo'
 
   constructor(
     private el: ElementRef<HTMLElement>
@@ -19,11 +20,17 @@ export class ErrorMsgDirective implements OnInit {
   ngOnInit(): void {
     console.log('NgOnInit en la directiva')
     this.setColor()
+    this.setMensaje()
   }
 
 
   setColor(): void {
     this.htmlElement.nativeElement.style.color = this.color
+    this.htmlElement.nativeElement.className = 'mt-2 text-sm'
+  }
+
+  setMensaje(): void {
+    this.htmlElement.nativeElement.innerText = this.mensaje
   }
 
 }
