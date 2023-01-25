2070cc4f3f111f5b6f556cf668736a4d66f8c208
Adrian Fernandez <adrianxtuu@gmail.com>
2023-01-25 10:30:32 -0400

    [UPDATE]

--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductosRoutingModule } from './productos-routing.module';
 import { AgregarComponent } from './pages/agregar/agregar.component';
@@ -11,7 +12,8 @@ import { AgregarComponent } from './pages/agregar/agregar.component';
   ],
   imports: [
     CommonModule,
-    ProductosRoutingModule
+    ProductosRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductosModule { }
