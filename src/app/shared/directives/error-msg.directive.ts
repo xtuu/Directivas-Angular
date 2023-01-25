15f5133ae41daa131846d0b4ddff824c4b3c1064
Adrian Fernandez <adrianxtuu@gmail.com>
2023-01-25 12:37:07 -0400

    [UPDATE]

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -25,6 +25,18 @@ export class ErrorMsgDirective implements OnInit, OnChanges {
     this.setMensaje()
   }
 
+  //cambio de validacion con setters
+
+  @Input() set valido(valor: boolean) {
+
+    if (valor) {
+      this.htmlElement.nativeElement.classList.add('hidden')
+    } else {
+      this.htmlElement.nativeElement.classList.remove('hidden')
+    }
+    
+  }
+
 
   constructor(
     private el: ElementRef<HTMLElement>
